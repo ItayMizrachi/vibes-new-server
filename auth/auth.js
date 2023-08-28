a4794433766b3b77bd50baa79d2bfb5922037225
ItayMizrachi <itaymiz98@gmail.com>
2023-08-28 19:33:05 +0300

    order

--- auth/auth.js
+++ auth/auth.js
@@ -19,21 +19,16 @@ exports.auth = (req, res, next) => {
 }
 
 exports.authAdmin = (req, res, next) => {
-    // בודק בכלל שטוקן נשלח בהדר
     let token = req.header("x-api-key");
     if (!token) {
         return res.status(401).json({ err: "You must send token to this endpoint " });
     }
     try {
         let decodeToken = jwt.verify(token, config.tokenSecret);
-        // בודק שהמשתמש הוא מסוג אדמין
         if (decodeToken.role != "admin") {
             return res.status(401).json({ err: "You must send admin token to this endpoint" });
         }
-        // req -> פרמטר של אובייקט שמשותף לכל הפונקציות בשרשור של הרואטר
         req.tokenData = decodeToken;
-        // נרצה להמשיך לפונקציה הבאה בשרשור של הראורט
-        // next() -> קורא לפונקציה הבאה בשרשור של הראוטר
         next();
     }
     catch (err) {
