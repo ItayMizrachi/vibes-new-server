537f03695fbf7c52a00f6cbb1ea0d3662c1cfe3f
ItayMizrachi <itaymiz98@gmail.com>
2023-08-28 13:19:29 +0300

    ss

--- routes/openai.js
+++ routes/openai.js
@@ -28,7 +28,7 @@ router.post("/completions", async (req, res) => {
 
                 
         // Set appropriate CORS headers
-        res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+        res.header('Access-Control-Allow-Origin', 'https://vibe-on-vibes.onrender.com/'); // Allow requests from any origin
         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
         res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         
